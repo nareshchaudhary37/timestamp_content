d0fc10c6d8fadda2f3c787b2560d08b1ed13ee2e
Naresh Chaudhary <naresh@quixom.com>
2016-07-06 15:58:06 +0530

    changed package name for installment

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from distutils.core import setup
 setup(
   name = 'user-friendly-date-maker',
-  packages = ['user-friendly-date-maker'], # this must be the same as the name above
+  packages = ['date_maker'], # this must be the same as the name above
   version = '0.1',
   description = 'The User friendly date maker project represents between passed date and current date time distance with appropricate message.',
   author = 'Naresh Chaudhary',
   author_email = 'nareshchaudhary37@gmail.com',
   url = 'https://github.com/nareshchaudhary37/user-friendly-date-maker', # use the URL to the github repo
-  download_url = 'https://github.com/nareshchaudhary37/user-friendly-date-maker', # I'll explain this in a second
   keywords = ['datetime', 'user friendly', 'just now', 'day ago'], # arbitrary keywords
   classifiers = [],
 )
