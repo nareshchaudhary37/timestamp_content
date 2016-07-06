233eb698dd8060ed1e434503b08ff0c76756442c
Naresh Chaudhary <naresh@quixom.com>
2016-07-06 17:13:29 +0530

    upgrade version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'user-friendly-date-maker',
   packages = ['date_maker'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.2',
   description = 'The User friendly date maker project represents between passed date and current date time distance with appropricate message.',
   author = 'Naresh Chaudhary',
   author_email = 'nareshchaudhary37@gmail.com',
