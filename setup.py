4b70ed9431ff93d607cd70ba648cafa3d0139698
Naresh Chaudhary <naresh@quixom.com>
2016-07-08 15:05:23 +0530

    Changed setup file

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from distutils.core import setup
 setup(
   name = 'user-friendly-date-maker',
   packages = ['date_maker'], # this must be the same as the name above
-  version = '0.2',
+  version = '0.3',
   description = 'The User friendly date maker project represents between passed date and current date time distance with appropricate message.',
   author = 'Naresh Chaudhary',
   author_email = 'nareshchaudhary37@gmail.com',
   url = 'https://github.com/nareshchaudhary37/user-friendly-date-maker', # use the URL to the github repo
+  download_url = 'https://github.com/nareshchaudhary37/user-friendly-date-maker/zipball/master/',
   keywords = ['datetime', 'user friendly', 'just now', 'day ago'], # arbitrary keywords
   classifiers = [],
 )
